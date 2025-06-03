9cfd316352137e52008f64fde2246b9e4e92d796
TuPM <tupm.citgroup@gmail.com>
2025-06-03 16:02:45 +0700

    add

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+# Di chuyen vao thu muc chua code
 cd ~/Desktop/doan/PI5
+
+# Bo tat ca thay doi o local, lay code moi nhat tren server
+git reset --hard HEAD
+git clean -fd
 git pull
+
+# Kich hoat moi truong ao python
 source venv/bin/activate
+
+# Chay chuong trinh python
 python app.py
