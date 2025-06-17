064e47e7c303136ea0794e44036ecd69288b3c9b
TuPM <tupm.citgroup@gmail.com>
2025-06-17 16:59:05 +0700

    add

--- PI5/app_realtime.py
+++ PI5/app_realtime.py
@@ -49,8 +49,8 @@ def camera_capture_loop(index):
         return
 
     # Set Full HD resolution
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     while True:
         ret, frame = cap.read()
